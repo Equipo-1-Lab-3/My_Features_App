9b02431b7c001a2ae727365b71f9d32cf882cec8
sebastianrpo <sebastian.restrepo7@udea.edu.co>
2023-03-24 18:01:26 -0500

    Conexión MainActivity con AreaHexagonoActivity

---
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/area_hexagono/AreaHexagonoActivity.kt
@@ -0,0 +1,12 @@
+package com.santiagotorres.myfeaturesapp.ui.area_hexagono
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.santiagotorres.myfeaturesapp.R
+
+class AreaHexagonoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area_hexagono)
+    }
+}
--- app/src/main/java/com/santiagotorres/myfeaturesapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/main/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.santiagotorres.myfeaturesapp.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.santiagotorres.myfeaturesapp.databinding.ActivityMainBinding
+import com.santiagotorres.myfeaturesapp.ui.area_hexagono.AreaHexagonoActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding =  ActivityMainBinding.inflate(layoutInflater)
-
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = mainBinding.root
         setContentView(view)
+
+        mainBinding.AHexagonoButton.setOnClickListener {
+            val intentAH = Intent(this, AreaHexagonoActivity::class.java)
+
+            startActivity(intentAH)
+        }
     }
 }
