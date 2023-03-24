d44de99e2b6f04af8ef539c50a8fdcd73040431a
santi <santiago.torresv>
2023-03-24 17:31:22 -0500

    Creación mainBinding en la pantalla principal

--- app/src/main/java/com/santiagotorres/myfeaturesapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/main/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.santiagotorres.myfeaturesapp.ui.main
 
-//..
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.santiagotorres.myfeaturesapp.R
+import com.santiagotorres.myfeaturesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding =  ActivityMainBinding.inflate(layoutInflater)
+
+        val view = mainBinding.root
+        setContentView(view)
     }
 }
