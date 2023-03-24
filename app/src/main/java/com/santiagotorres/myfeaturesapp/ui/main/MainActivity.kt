867466dcb1dc3a65386efdd0439c59f85d6a493a
ydpajaro <yeiner.pajaro@udea.edu.co>
2023-03-24 17:48:54 -0500

    Creación de la conexión al botón perimetro circulo

--- app/src/main/java/com/santiagotorres/myfeaturesapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/main/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.santiagotorres.myfeaturesapp.ui.main
 
 //..
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.santiagotorres.myfeaturesapp.R
+import com.santiagotorres.myfeaturesapp.databinding.ActivityMainBinding
+import com.santiagotorres.myfeaturesapp.ui.perimetro_circulo.PerimetroCirculoActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding =  ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        mainBinding.PCirculoButton.setOnClickListener {
+            val  intentPC =  Intent(this,PerimetroCirculoActivity::class.java)
+
+            startActivity(intentPC)
+        }
+
+
     } // Yeiner D. Pájaro
 }
