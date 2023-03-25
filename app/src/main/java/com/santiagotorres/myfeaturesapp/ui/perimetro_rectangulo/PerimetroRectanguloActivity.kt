8d364b013034cfdacd19cf7592de7e9b524aaa07
santi <santiago.torresv>
2023-03-25 11:41:22 -0500

    Creación de la interfaz para el cálculo del perímetro del rectángulo sin viewmodel aún

--- app/src/main/java/com/santiagotorres/myfeaturesapp/ui/perimetro_rectangulo/PerimetroRectanguloActivity.kt
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/perimetro_rectangulo/PerimetroRectanguloActivity.kt
@@ -3,10 +3,35 @@ package com.santiagotorres.myfeaturesapp.ui.perimetro_rectangulo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.santiagotorres.myfeaturesapp.R
+import com.santiagotorres.myfeaturesapp.databinding.ActivityPerimetroRectanguloBinding
+import java.text.DecimalFormat
 
 class PerimetroRectanguloActivity : AppCompatActivity() {
+
+    private lateinit var converterBinding: ActivityPerimetroRectanguloBinding
+    private var valor_base : Double = 0.0
+    private var valor_altura : Double = 0.0
+    private var valor_perimetro : Double = 0.0
+    val df = DecimalFormat("###,###,###.##")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perimetro_rectangulo)
+        converterBinding = ActivityPerimetroRectanguloBinding.inflate(layoutInflater)
+
+        val view = converterBinding.root
+        setContentView(view)
+
+
+        converterBinding.calculateButton.setOnClickListener{
+            valor_base = converterBinding.BaseInput.text.toString().toDouble()
+            valor_altura = converterBinding.AlturaInput.text.toString().toDouble()
+
+            valor_perimetro = (2 * valor_base) + (2 * valor_altura)
+
+            converterBinding.resultText.text = "El perimetro es ${df.format(valor_perimetro)} cm"
+
+
+        }
+
     }
 }
