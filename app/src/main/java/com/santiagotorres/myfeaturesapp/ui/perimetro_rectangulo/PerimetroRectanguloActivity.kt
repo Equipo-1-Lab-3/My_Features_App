fc65aec22b97596f6050a81730a904d14edcaf89
santi <santiago.torresv>
2023-03-25 13:44:00 -0500

    Creación del viewmodel para calcular el perimetro y verificar errores (aún falta que reinicie el print de abajo)

--- app/src/main/java/com/santiagotorres/myfeaturesapp/ui/perimetro_rectangulo/PerimetroRectanguloActivity.kt
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/perimetro_rectangulo/PerimetroRectanguloActivity.kt
@@ -2,35 +2,43 @@ package com.santiagotorres.myfeaturesapp.ui.perimetro_rectangulo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.santiagotorres.myfeaturesapp.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.santiagotorres.myfeaturesapp.databinding.ActivityPerimetroRectanguloBinding
 import java.text.DecimalFormat
 
 class PerimetroRectanguloActivity : AppCompatActivity() {
 
     private lateinit var converterBinding: ActivityPerimetroRectanguloBinding
-    private var valor_base : Double = 0.0
-    private var valor_altura : Double = 0.0
-    private var valor_perimetro : Double = 0.0
+    private lateinit var perimetroRectanguloViewModel: PerimetroRectanguloViewModel
+
+    private var valorbase : Double = 0.0
+    private var valoraltura : Double = 0.0
     val df = DecimalFormat("###,###,###.##")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         converterBinding = ActivityPerimetroRectanguloBinding.inflate(layoutInflater)
 
+
+        perimetroRectanguloViewModel = ViewModelProvider(this)[PerimetroRectanguloViewModel::class.java]
+
+
         val view = converterBinding.root
         setContentView(view)
 
 
         converterBinding.calculateButton.setOnClickListener{
-            valor_base = converterBinding.BaseInput.text.toString().toDouble()
-            valor_altura = converterBinding.AlturaInput.text.toString().toDouble()
-
-            valor_perimetro = (2 * valor_base) + (2 * valor_altura)
-
-            converterBinding.resultText.text = "El perimetro es ${df.format(valor_perimetro)} cm"
 
 
+            val calcularVerificar = Observer<Double> {result->
+                converterBinding.resultText.text = "El perimetro es ${df.format(result)} cm"
+
+            }
+            perimetroRectanguloViewModel.Resultado.observe(this, calcularVerificar)
+
+            perimetroRectanguloViewModel.calcularPerimetro (converterBinding.BaseInput.text.toString(),
+                converterBinding.AlturaInput.text.toString(), converterBinding.uniqueLayout)
         }
 
     }
---
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/perimetro_rectangulo/PerimetroRectanguloViewModel.kt
@@ -0,0 +1,38 @@
+package com.santiagotorres.myfeaturesapp.ui.perimetro_rectangulo
+
+import android.widget.LinearLayout
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.android.material.snackbar.Snackbar
+
+
+class PerimetroRectanguloViewModel : ViewModel() {
+
+
+    val Resultado: MutableLiveData<Double> by lazy {
+        MutableLiveData<Double>()
+    }
+
+    val emptyText: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    fun calcularPerimetro (baseInput: String, alturaInput : String, layout: LinearLayout) {
+
+        if (baseInput.isEmpty() || baseInput == "0" || alturaInput.isEmpty() || alturaInput=="0"){
+            emptyText.value = "Tiene algún valor vacío o en 0"
+
+            Snackbar.make(layout,
+                emptyText.value.toString(),
+                Snackbar.LENGTH_INDEFINITE).setAction("Aceptar") {
+            }.show()
+
+        }
+        else{
+            var base = baseInput.toDouble()
+            var altura = alturaInput.toDouble()
+
+            Resultado.value = (2*base) + (2*altura)
+        }
+    }
+}
