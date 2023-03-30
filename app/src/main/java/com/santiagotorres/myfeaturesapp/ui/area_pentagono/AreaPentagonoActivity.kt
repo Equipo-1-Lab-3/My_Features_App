11a733ac72bb7f8e62839bae313d64dc64b83248
Katherin <katherin.henao@udea.edu.co>
2023-03-30 15:47:18 -0500

    Conexión Botón AreaPentagono con el MainActivity

---
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/area_pentagono/AreaPentagonoActivity.kt
@@ -0,0 +1,12 @@
+package com.santiagotorres.myfeaturesapp.ui.area_pentagono
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.santiagotorres.myfeaturesapp.R
+
+class AreaPentagonoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area_pentagono)
+    }
+}
