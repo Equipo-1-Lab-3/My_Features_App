c0dd6682f98318594fbde7d92a08e9eacf3b7493
ydpajaro <yeiner.pajaro@udea.edu.co>
2023-03-30 15:27:09 -0500

    Creación del ViewModel, solución a entradas para valores vacíos y con puntos.

---
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/perimetro_circulo/PerimetroCirculoActivity.kt
@@ -0,0 +1,42 @@
+package com.santiagotorres.myfeaturesapp.ui.perimetro_circulo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.santiagotorres.myfeaturesapp.R
+import com.santiagotorres.myfeaturesapp.databinding.ActivityMainBinding
+import com.santiagotorres.myfeaturesapp.databinding.ActivityPerimetroCirculoBinding
+import java.text.DecimalFormat
+
+class PerimetroCirculoActivity : AppCompatActivity() {
+
+    private lateinit var perimetroCirculoBinding: ActivityPerimetroCirculoBinding
+    private lateinit var perimetroCirculoViewModel: PerimetroCirculoViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        perimetroCirculoBinding = ActivityPerimetroCirculoBinding.inflate(layoutInflater)
+
+        perimetroCirculoViewModel = ViewModelProvider(this)[PerimetroCirculoViewModel::class.java]
+
+
+
+
+        val mensajeObserver = Observer<String>{mensaje ->
+            perimetroCirculoBinding.totalTextView.text=mensaje
+        }
+        perimetroCirculoViewModel.mensaje.observe(this, mensajeObserver)
+
+        perimetroCirculoBinding.calcularPerimetroButton.setOnClickListener {
+            perimetroCirculoViewModel.calculoperimetro(perimetroCirculoBinding.RadioInput.text.toString())
+        }
+
+        val view = perimetroCirculoBinding.root
+        setContentView(view)
+    }
+
+}
---
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/perimetro_circulo/PerimetroCirculoViewModel.kt
@@ -0,0 +1,30 @@
+package com.santiagotorres.myfeaturesapp.ui.perimetro_circulo
+
+import android.widget.LinearLayout
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.android.material.snackbar.Snackbar
+
+class PerimetroCirculoViewModel: ViewModel() {
+
+   val mensaje: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    fun calculoperimetro(inputRadio: String) {
+
+        if (inputRadio == "" ) {
+            mensaje.value = "DESCRIPCIÓN: El campo debe contener un valor."
+        }else if (inputRadio == "." ){
+            mensaje.value = "DESCRIPCIÓN: El campo debe ser un número."
+        }    else {
+            var total = 2*Math.PI*inputRadio.toDouble()
+
+            mensaje.value = "El área del tríangulo es: ${String.format("%.2f",total.toDouble())}"
+
+
+
+        }
+    }
+
+}
