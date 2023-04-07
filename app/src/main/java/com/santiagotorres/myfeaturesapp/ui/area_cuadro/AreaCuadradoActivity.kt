800f3650a6363314d576c373e3c3057bca582a0a
camilomazo99 <camilo.mazo2@udea.edu.co>
2023-04-06 22:53:12 -0500

    Se termina la actividad para medir el area del cuadrado.

--- app/src/main/java/com/santiagotorres/myfeaturesapp/ui/area_cuadro/AreaCuadradoActivity.kt
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/area_cuadro/AreaCuadradoActivity.kt
@@ -2,11 +2,29 @@ package com.santiagotorres.myfeaturesapp.ui.area_cuadro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 import com.santiagotorres.myfeaturesapp.R
+import com.santiagotorres.myfeaturesapp.databinding.ActivityAreaCuadradoBinding
 
 class AreaCuadradoActivity : AppCompatActivity() {
+    private lateinit var areaCuadradoBinding: ActivityAreaCuadradoBinding
+    private lateinit var areaCuadradoViewModel: AreaCuadradoViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_area_cuadrado)
+        areaCuadradoBinding=ActivityAreaCuadradoBinding.inflate(layoutInflater)
+        areaCuadradoViewModel=ViewModelProvider(this)[AreaCuadradoViewModel::class.java]
+        val view=areaCuadradoBinding.root
+        setContentView(view)
+
+        val calculoObserver = Observer<Double>{resultado->
+            areaCuadradoBinding.cuadradoResultText.text=resultado.toString()
+        }
+        areaCuadradoViewModel.resultado.observe(this,calculoObserver)
+
+        areaCuadradoBinding.cuadradoButton.setOnClickListener{
+            areaCuadradoViewModel.calculo(areaCuadradoBinding.alturaInput.text.toString().toDouble(),areaCuadradoBinding.anchoInput.text.toString().toDouble())
+        }
     }
 }
--- app/src/main/java/com/santiagotorres/myfeaturesapp/ui/area_cuadro/AreaCuadradoViewModel.kt
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/area_cuadro/AreaCuadradoViewModel.kt
@@ -1,7 +1,14 @@
 package com.santiagotorres.myfeaturesapp.ui.area_cuadro
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AreaCuadradoViewModel: ViewModel() {
+    val resultado : MutableLiveData<Double> by lazy{
+        MutableLiveData<Double>()
+    }
 
+    fun calculo(vAlto:Double,vAncho:Double){
+        resultado.value=vAlto*vAncho
+    }
 }
