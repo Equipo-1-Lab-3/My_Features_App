64b0bcd1ea60ac9056ab6307eca1813a7758f5e3
Katherin <katherin.henao@udea.edu.co>
2023-04-11 10:50:28 -0500

    Nueva Rama creada- para solucion issue con merge

---
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/main/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.santiagotorres.myfeaturesapp.ui.main
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.santiagotorres.myfeaturesapp.databinding.ActivityMainBinding
+import com.santiagotorres.myfeaturesapp.ui.AreaPentagono.AreaPentagonoActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding =  ActivityMainBinding.inflate(layoutInflater)
+
+        val view = mainBinding.root
+        setContentView(view)
+
+        mainBinding.APentagonoButton.setOnClickListener {
+            val  intentAP =  Intent(this, AreaPentagonoActivity::class.java)
+
+            startActivity(intentAP)
+        }
+    }
+}
