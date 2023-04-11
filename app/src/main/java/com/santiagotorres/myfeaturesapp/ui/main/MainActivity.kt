e9fecf741cc2ff0e1012a4b93b1b54bc2c864d07
martin_barbosa <martin.barbosa@udea.edu.co>
2023-04-10 20:20:27 -0500

    Se organiza el view Model, soluciona errores del usuario al ingresar campos vacíos y campos con puntos version 2

--- app/src/main/java/com/santiagotorres/myfeaturesapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/santiagotorres/myfeaturesapp/ui/main/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.santiagotorres.myfeaturesapp.ui.main
 
 //..
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.santiagotorres.myfeaturesapp.R
+import com.santiagotorres.myfeaturesapp.databinding.ActivityMainBinding
+import com.santiagotorres.myfeaturesapp.ui.area_triangulo.AreaTrianguloActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding =  ActivityMainBinding.inflate(layoutInflater)
+
+        val view = mainBinding.root
+        setContentView(view)
+        mainBinding.ATrianguloButton.setOnClickListener {
+            val intentAT = Intent(this,AreaTrianguloActivity::class.java)
+            startActivity(intentAT)
+        }
         //si
     }
 }
